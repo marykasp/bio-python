9fd04c06e6f6e98244f154c39352a0c6548b2182
marykasp <kasparian.mary@gmail.com>
2024-02-01 11:11:07 -0600

    exercise: create 2 new files that store the coding and noncoding sequences from the genomic dna in the genomic_dna.txt file

---
+++ ch3-files/exercises/genomic_data.py
@@ -0,0 +1,22 @@
+# Write a program that will split the genomic DNA into coding and non-coding parts (exons and introns)
+# Write the sequence to two separate files
+
+# Open and read the genomic dna text file
+dna_file = open("genomic_data.txt")
+dna_seq = dna_file.read().rstrip("\n")
+
+# Print the full DNA sequence
+print("DNA sequence: " + dna_seq)
+
+# Split into exons and introns
+exon1 = dna_seq[0:64] # 1 - 63 base
+exon2 = dna_seq[91:] # base 91 to end of sequence
+intron = dna_seq[64:91]
+
+coding_file = open("coding_dna.txt", "w")
+coding_file.write(exon1 + exon2)
+
+noncoding_file = open("noncoding_dna.txt", "w")
+noncoding_file.write(intron)
+
+# Check the new files
