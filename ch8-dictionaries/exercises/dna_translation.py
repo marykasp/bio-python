6ec86dc84ee5645200d1f81f212b7d285a88b9ed
marykasp <kasparian.mary@gmail.com>
2024-02-13 11:41:05 -0600

    exercise: account for sequences that are not multiplies of 3, need to determing last position of start in range
    Also need a default value for when retrieving aa from dict, since non
    bases may be included in sequence

--- ch8-dictionaries/exercises/dna_translation.py
+++ ch8-dictionaries/exercises/dna_translation.py
@@ -18,7 +18,7 @@ gencode = {
 
 # DNA to split into codons and turn into a protein sequence
 dna = "ATGTTCGGT"
-print("dna length: " + str(len(dna)))
+# print("dna length: " + str(len(dna)))
 # substring notation
 codon1 = dna[0:3]
 codon2 = dna[3:6]
@@ -27,15 +27,23 @@ codon3 = dna[6:]
 
 # use range to generate a sequence of numbers starting from 0, stepwise of 3
 # start at 0, stop at 3 less than length of dna sequence
-protein = ""
-
-for start_pos in range(0, 7, 3):
-  # 0, 3, 6
-  codon = dna[start_pos:start_pos+3]
-  aa = gencode.get(codon)
-  print("codon: " + codon)
-  print("amino acid code is: " + aa)
-
-  # string concatenation
-  protein = protein + aa
   
+def translate_dna(dna):
+  protein = ""
+
+  last_pos = len(dna) - 2 
+  for start_pos in range(0, last_pos, 3):
+    codon = dna[start_pos:start_pos+3]
+    aa = gencode.get(codon, "X")
+    print("codon: " + codon)
+    # string concatenation
+    protein = protein + aa
+
+  return protein
+
+print(translate_dna(dna))
+print(translate_dna("ATGTTCGGTA"))
+
+# trying sequence with undetermined base
+print(translate_dna("ATGTTNCGGT"))
+
